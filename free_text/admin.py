79c85933baf57aa8bebd05c1232ee8e12a71525c
Peter Baumgartner <pete@lincolnloop.com>
2008-12-02 21:14:55 -0700

    friendly admin

--- free_text/admin.py
+++ free_text/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from free_text.models import FreeText
  
-admin.site.register(FreeText)
+class FreeTextAdmin(admin.ModelAdmin):
+    ordering = ['slug',]
+    list_display = ('slug', 'header', 'content')
+    search_fields = ('slug', 'header', 'content')
+
+admin.site.register(FreeText, FreeTextAdmin)
