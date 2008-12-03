12b3e572a4b8227855090d4169b15f823aa038ff
Peter Baumgartner <pete@lincolnloop.com>
2008-12-02 21:07:26 -0700

    add header

--- free_text/models.py
+++ free_text/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 class FreeText(models.Model):
     """
-    A FreeText is a piece of content associated with a unique key that 
-    can be inserted into any template with the use of a special template
-    tag
+    A piece of content associated with a unique slug that can be inserted 
+    into any template with the use of a special template tag.
+    
     """
     slug = models.CharField(max_length=255, unique=True
                 help_text="A unique name for this content")
+    header = models.CharField(blank=True, null=True, max_length=255,
+                help_text="An optional header for this content")
     content = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
