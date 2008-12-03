5ec7460b692915f7124aa402fd70a74f8d4a2792
Peter Baumgartner <pete@lincolnloop.com>
2008-12-02 21:03:08 -0700

    model cleanup

--- free_text/models.py
+++ free_text/models.py
@@ -2,18 +2,13 @@ from django.db import models
 
 class FreeText(models.Model):
     """
-    A FreeText is a piece of content associated
-    with a unique key that can be inserted into
-    any template with the use of a special template
+    A FreeText is a piece of content associated with a unique key that 
+    can be inserted into any template with the use of a special template
     tag
     """
-    key = models.CharField('key', help_text="A unique name for this FreeText of content", blank=False, maxlength=255, unique=True)
-    content = models.TextField('content', blank=True)
-    active = models.BooleanField("active", default=False)
-
-    class Meta:
-        verbose_name = 'free text'
-        verbose_name_plural = 'free texts'
+    key = models.CharField(max_length=255, unique=True
+                help_text="A unique name for this content")
+    content = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
         return u"%s" % (self.key,)
