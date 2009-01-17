7cbb030876ac94b3a73a964dd3d1dad32aa66833
Horst Gutmann <zerok@zerokspot.com>
2009-01-17 23:19:49 +0100

    You can now also pass a variable to the templatetag.
    It is now also possible to pass the freetext-slug to the templatetag inside
    a template variable. If you leave out quotes when passing the name, it is
    treated as a variable.

--- free_text/templatetags/freetext.py
+++ free_text/templatetags/freetext.py
@@ -6,8 +6,12 @@ into a template.
 It accepts 2 parameter:
 
     slug
-        The slug/key of the text (for example 'contact_help'). This slug has
-        to be passed with quotes to the templatetag.
+        The slug/key of the text (for example 'contact_help'). There are two
+        ways you can pass the slug to the templatetag: (1) by its name or 
+        (2) as a variable.
+        
+        If you want to pass it by name, you have to use quotes on it. 
+        Otherwise just use the variable name.
 
     cache_time
         The number of seconds that text should get cached after it has been
@@ -22,6 +26,7 @@ Example::
     ...
     
     {% freetext 'contact_help' %}
+    {% freetext name_in_variable %}
     
 
 """
@@ -38,6 +43,8 @@ CACHE_PREFIX = "freetext_"
 def do_get_freetext(parser, token):
     # split_contents() knows not to split quoted strings.
     tokens = token.split_contents()
+    is_variable = False
+    real_slug = None
     if len(tokens) < 2 or len(tokens) > 3:
         raise template.TemplateSyntaxError, "%r tag should have either 2 or 3 arguments" % (tokens[0],)
     if len(tokens) == 2:
@@ -47,21 +54,29 @@ def do_get_freetext(parser, token):
         tag_name, slug, cache_time = tokens
     # Check to see if the slug is properly double/single quoted
     if not (slug[0] == slug[-1] and slug[0] in ('"', "'")):
-        raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tag_name
+        is_variable = True
+        real_slug = slug
+    else:
+        real_slug = slug[1:-1]
     # Send slug without quotes and caching time
-    return FreeTextNode(slug[1:-1], cache_time)
+    return FreeTextNode(real_slug, is_variable, cache_time)
     
 class FreeTextNode(template.Node):
-    def __init__(self, slug, cache_time=0):
+    def __init__(self, slug, is_variable, cache_time=0):
        self.slug = slug
+       self.is_variable = is_variable
        self.cache_time = cache_time
     
     def render(self, context):
+        if self.is_variable:
+            real_slug = template.Variable(self.slug).resolve(context)
+        else:
+            real_slug = self.slug
         try:
-            cache_key = CACHE_PREFIX + self.slug
+            cache_key = CACHE_PREFIX + real_slug
             c = cache.get(cache_key)
             if c is None:
-                c = FreeText.objects.get(slug=self.slug)
+                c = FreeText.objects.get(slug=real_slug)
                 cache.set(cache_key, c, int(self.cache_time))
             content = c.content
         except FreeText.DoesNotExist:
