9ccc9aa90ba6950c689f193a50e707d71a678894
Horst Gutmann <zerok@zerokspot.com>
2009-01-20 10:50:02 +0100

    Added some i18n-support to the models.py

--- flatblocks/models.py
+++ flatblocks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class FlatBlock(models.Model):
     """
@@ -7,9 +8,9 @@ class FlatBlock(models.Model):
     
     """
     slug = models.CharField(max_length=255, unique=True,
-                help_text="A unique name used for reference in the templates")
+                help_text=_("A unique name used for reference in the templates"))
     header = models.CharField(blank=True, null=True, max_length=255,
-                help_text="An optional header for this content")
+                help_text=_("An optional header for this content"))
     content = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
