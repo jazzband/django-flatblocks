4343988a1f758456e3d5ce8e4478f50428e5f86f
Horst Gutmann <zerok@zerokspot.com>
2009-01-22 22:05:29 +0100

    Rephrased the docstring of the FlatBlock model

--- flatblocks/models.py
+++ flatblocks/models.py
@@ -3,9 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 
 class FlatBlock(models.Model):
     """
-    A piece of content associated with a unique slug that can be inserted 
-    into any template with the use of a special template tag.
-    
+    Think of a flatblock as a flatpage but for just part of a site. It's
+    basically a piece of content with a given name (slug) and an optional
+    title (header) which you can, for example, use in a sidebar of a website.
     """
     slug = models.CharField(max_length=255, unique=True,
                 help_text=_("A unique name used for reference in the templates"))
