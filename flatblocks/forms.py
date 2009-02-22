ac67423fc716daf20a5194a8ecf2fd55cca6bc81
Horst Gutmann <zerok@zerokspot.com>
2009-02-22 17:33:28 +0100

    Added a simple view for editing flatblocks
    This view doesn't have any permission restrictions by default. You will have to add them through your URL configuration (which is also the reason why there is no urls.py in this app ;-)). For details on how to do that please take a look at the docstring of flatblocks.views.edit.

---
+++ flatblocks/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+
+from flatblocks.models import FlatBlock
+
+class FlatBlockForm(ModelForm):
+    class Meta:
+        model = FlatBlock
+        exclude = ('slug', )
---
+++ flatblocks/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
+from django.utils.translation import ugettext as _
+
+from flatblocks.models import FlatBlock
+from flatblocks.forms import FlatBlockForm
+
+
+def edit(request, pk, modelform_class=FlatBlockForm, permission_check=None):
+    """
+    This view provides a simple editor implementation for flatblocks.
+
+    There are two customization hooks. First of all you can specify your own
+    ModelForm class by passing it to the view using the ``modelform_class``
+    keyword-argument.
+
+    The other entry point helps you check permissions: Pass a simple function
+    via the ``permission_check`` keyword-argument in order to check 
+    permissions on the flatblock-level::
+        
+        def my_perm_check(request, flatblock):
+            return request.user.is_staff
+
+        # ...
+
+        urlpatterns('flatblocks.views',
+            url('flatblocks/(?P<pk>\d+)/edit/$', 'edit', kwargs={'permission_check': my_perm_check}),
+            )
+
+    The contract here is pretty simple: If the function returns False, the
+    view will return HttpResponseForbidden. Otherwise it will pass.  So if you
+    want to do some fancy redirects if the permissions are wrong, return your
+    own HttpResponse-object/-subclass. 
+
+    If everything is alright with the permissions, simply return True.
+    """
+    flatblock = get_object_or_404(FlatBlock, pk=pk)
+    if permission_check is not None:
+        permcheck_result = permission_check(request, flatblock)
+        if permcheck_result is False:
+            return HttpResponseForbidden(_('You are not allowed to edit this flatblock'))
+        if isinstance(permcheck_result, HttpResponse):
+            return permcheck_result
+
+    session_key = 'flatblock.origin.%d' % (int(pk), )
+    if request.method == 'POST':
+        origin = request.session.get(session_key, 
+                request.META.get('HTTP_REFERER', '/'))
+        form = modelform_class(request.POST, instance=flatblock)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.slug = flatblock.slug
+            instance.save()
+            del request.session[session_key]
+            return HttpResponseRedirect(origin)
+    else:
+        origin = request.META.get('HTTP_REFERER', '/')
+        # Don't set origin to this view's url no matter what
+        origin = origin == request.get_full_path() and request.session.get(session_key, '/') or origin
+        form = modelform_class(instance=flatblock)
+        request.session[session_key] = origin
+    return render_to_response('flatblocks/edit.html', RequestContext(request, {
+        'form': form,
+        'origin': origin,
+        'flatblock': flatblock,
+        }))
