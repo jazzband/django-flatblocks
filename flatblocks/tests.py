eeb5bd0a9b541946ec4783f02fc947bdfec1dd61
Mikhail Korobov <kmike84@gmail.com>
2010-09-11 06:38:22 +0600

    Simplify test running. Fix tests for django 1.2.
    Tests now can be executed using runtests.py without installing django-flatblocks to PYTHONPATH.

--- flatblocks/tests.py
+++ flatblocks/tests.py
@@ -20,9 +20,9 @@ class BasicTests(TestCase):
         self.testblock.content = 'CONTENT'
         self.testblock.save()
         self.admin = User.objects.create_superuser('admin', 'admin@localhost', 'adminpwd')
-    
+
     def testURLConf(self):
-        self.assertEquals(self.client.get('/edit/1/').template.name, 'admin/login.html')
+        self.assertEquals(self.client.get('/edit/1/').template[0].name, 'admin/login.html')
         self.client.login(username='admin', password='adminpwd')
         self.assertEquals(self.client.get('/edit/1/').template.name, 'flatblocks/edit.html')
 
@@ -31,14 +31,14 @@ class BasicTests(TestCase):
         Tests if FlatBlock.save() resets the cache.
         """
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" 60 %}')
-        tpl.render({})
+        tpl.render(template.Context({}))
         name = '%sblock' % CACHE_PREFIX
         self.assertNotEquals(None, cache.get(name))
         block = models.FlatBlock.objects.get(slug='block')
         block.header = 'UPDATED'
         block.save()
         self.assertEquals(None, cache.get(name))
-        
+
     def testSaveKwargs(self):
         block = models.FlatBlock()
         block.slug = 'missing'
@@ -60,45 +60,46 @@ class TagTests(unittest.TestCase):
     def testLoadingTaglib(self):
         """Tests if the taglib defined in this app can be loaded"""
         tpl = template.Template('{% load flatblock_tags %}')
-        tpl.render({})
+        tpl.render(template.Context({}))
 
     def testMissingBlock(self):
         """Tests if a missing block will simply return an empty string"""
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "missing_block" %}')
-        self.assertEqual('', tpl.render({}).strip())
+        self.assertEqual('', tpl.render(template.Context({})).strip())
 
     def testExistingPlain(self):
         tpl = template.Template('{% load flatblock_tags %}{% plain_flatblock "block" %}')
-        self.assertEqual(u'CONTENT', tpl.render({}).strip()) 
+        self.assertEqual(u'CONTENT', tpl.render(template.Context({})).strip())
 
     def testExistingTemplate(self):
         expected = """<div class="flatblock block-block">
-    
+
     <h2 class="title">HEADER</h2>
-    
+
     <div class="content">CONTENT</div>
-</div>"""
+</div>
+"""
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" %}')
-        self.assertEqual(expected, tpl.render({}))
+        self.assertEqual(expected, tpl.render(template.Context({})))
 
     def testUsingMissingTemplate(self):
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" using "missing_template.html" %}')
-        exception = template.TemplateDoesNotExist
-        self.assertRaises(exception, tpl.render, {})
+        exception = template.TemplateSyntaxError
+        self.assertRaises(exception, tpl.render, template.Context({}))
 
     def testSyntax(self):
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" %}')
-        tpl.render({})
+        tpl.render(template.Context({}))
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" 123 %}')
-        tpl.render({})
+        tpl.render(template.Context({}))
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" using "flatblocks/flatblock.html" %}')
-        tpl.render({})
+        tpl.render(template.Context({}))
         tpl = template.Template('{% load flatblock_tags %}{% flatblock "block" 123 using "flatblocks/flatblock.html" %}')
-        tpl.render({})
+        tpl.render(template.Context({}))
 
     def testBlockAsVariable(self):
         tpl = template.Template('{% load flatblock_tags %}{% flatblock blockvar %}')
-        tpl.render({'blockvar': 'block'})
+        tpl.render(template.Context({'blockvar': 'block'}))
 
     def tearDown(self):
         self.testblock.delete()
--- test_project/settings.py
+++ test_project/settings.py
@@ -1,17 +1,22 @@
 import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(PROJECT_ROOT))
+
 DEBUG=True
 TEMPLATE_DEBUG=True
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = '/tmp/flatblocks.db'
 INSTALLED_APPS = (
-    'django.contrib.auth', 
+    'django.contrib.auth',
     'django.contrib.admin',
-    'django.contrib.contenttypes', 
-    'django.contrib.sessions', 
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'flatblocks',
 )
 LANGUAGE_CODE="no"
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'templates'),
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
-ROOT_URLCONF = 'test_project.urls'
+ROOT_URLCONF = 'urls'
