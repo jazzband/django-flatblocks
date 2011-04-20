eb8e3c570312b777db4495deb0b591a463a73b95
Horst Gutmann <zerok@zerokspot.com>
2011-04-20 21:31:04 +0200

    Do not display content in admin list.
    Remove the 'content' from the list in admin manager interface. It was
    clobbering up the list.
    
    Patch provided by Fladischer Michael <FladischerMichael@fladi.at>

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -3,7 +3,7 @@ from flatblocks.models import FlatBlock
  
 class FlatBlockAdmin(admin.ModelAdmin):
     ordering = ['slug',]
-    list_display = ('slug', 'header', 'content')
+    list_display = ('slug', 'header')
     search_fields = ('slug', 'header', 'content')
 
 admin.site.register(FlatBlock, FlatBlockAdmin)
