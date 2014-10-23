0979502db5f8b61be4005f0b63afb85d7134949a
funkybob <curtis@tinbrain.net>
2014-10-24 09:08:23 +1100

    Convert to new tests structure

---
+++ runtests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Adapted from https://raw.githubusercontent.com/hzy/django-polarize/master/runtests.py
+
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        INSTALLED_APPS=(
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.auth',
+            'django.contrib.admin',
+            'flatblocks',
+            'tests',
+        ),
+        ROOT_URLCONF = 'tests.urls'
+    )
+
+
+def runtests():
+    argv = sys.argv[:1] + ['test', 'tests']
+    execute_from_command_line(argv)
+
+
+if __name__ == '__main__':
+    runtests()
--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,11 @@
-try:
-    from django.conf.urls import patterns, url, include
-except ImportError:  # Django < 1.4
-    from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, url, include
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import admin
-from . import views
+from django.shortcuts import render
 
-admin.autodiscover()
 
 urlpatterns = patterns('',
     url('^flatblocks/', include("flatblocks.urls")),
     url('^admin/', include(admin.site.urls)),
-    url('^/?', views.index),
+    url('^/?', render, {'template_name': 'index.html'}),
 )
