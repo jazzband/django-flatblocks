da585b0831014c00eb34d7f334a74a99aa7f615a
Curtis <curtis@tinbrain.net>
2015-01-25 13:13:17 +1100

    Update to modern urls imports

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import admin
 from django.shortcuts import render
-
 
 urlpatterns = patterns('',
     url('^flatblocks/', include("flatblocks.urls")),
