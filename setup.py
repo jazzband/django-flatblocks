9913e1ed57717af248370149b6f69127bc4a48f0
Curtis <curtis@tinbrain.net>
2015-01-25 13:33:37 +1100

    Update author and trove details

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     long_description=open('README.rst').read(),
     keywords='django apps',
     license='New BSD License',
-    author='Horst Gutmann',
-    author_email='zerok@zerokspot.com',
+    author='Horst Gutmann, Curtis Maloney',
+    author_email='curtis@tinbrain.net',
     url='http://github.com/funkybob/django-flatblocks/',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
