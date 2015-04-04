96a377d20740893325e9e74dd2762c941e281f05
Curtis Maloney <curtis@tinbrain.net>
2015-04-04 14:50:22 +1100

    Bump version to 0.9.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-flatblocks',
-    version='0.9',
+    version='0.9.1',
     description='django-flatblocks acts like django.contrib.flatpages but '
                 'for parts of a page; like an editable help box you want '
                 'show alongside the main content.',
